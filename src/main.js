ef9a8a229f8dcbdea5a27b6e7f9067b048cd8a81
Xcedeveloper <xceldeveloper@gmail.com>
2021-12-10 13:40:21 +0100

    removed redundant lib

--- src/main.js
+++ src/main.js
@@ -4,12 +4,19 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import DateFilter from "./filters/date";
-import { ValidationProvider } from "vee-validate/dist/vee-validate.full.esm";
-import { ValidationObserver } from "vee-validate";
-import { VueFormBuilderPlugin } from "v-form-builder";
+import {
+    ValidationProvider
+} from "vee-validate/dist/vee-validate.full.esm";
+import {
+    ValidationObserver
+} from "vee-validate";
+import {
+    VueFormBuilderPlugin
+} from "v-form-builder";
 import "../src/assets/styles/formbuilder.css";
 import "v-form-builder/dist/v-form-builder.css";
 import JsonCSV from "vue-json-csv";
+
 
 import SimpleLineIcons from "vue-simple-line";
 Vue.config.productionTip = false;
@@ -26,6 +33,7 @@ Vue.component("downloadCsv", JsonCSV);
 
 // install now
 Vue.use(VueFormBuilderPlugin);
+
 
 //register date filter
 Vue.filter("date", DateFilter);
