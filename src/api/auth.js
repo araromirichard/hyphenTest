b2be35e71389eb325aeba0037a77a39e1d2343e3
Xcedeveloper <xceldeveloper@gmail.com>
2021-12-10 18:08:34 +0100

    auth endpoint service

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,9 +3,38 @@ import api from "./Api";
 export default {
   login(credentials) {
     return api().post("/auth/local", credentials);
+
+    // {
+    //   "identifier":"nulled08@ahem.email",
+    //   "password":"password"
+    // }
   },
 
   registerUser(credentials){
       return api().post("/auth/register", credentials);
+
+      // {
+      //   "username":"gdi",
+      //   "email":"sa@muhhhlaa.me",
+      //   "password":"password",
+      //   "Firstname":"Samson",
+      //   "Lastname":"Aligba"
+      //   }
+  },
+
+  registerCoWorkerUser(credentials){
+    return api().post('/auth/local/register',credentials);
+
+    // {
+    //   "username":"blue014",
+    //   "email":"blue007@ahem.email",
+    //   "password":"password",
+    //   "role": "coworker",
+    //   "Firstname":"Olu",
+    //   "Lastname":"Jacobs",
+    //   "organizations":{
+    //       "id":7
+    //   }
+    //   }
   }
 };
