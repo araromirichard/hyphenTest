764fb48c1104be2052427c498096fab108127691
Xcedeveloper <xceldeveloper@gmail.com>
2022-02-14 01:49:52 +0100

    ..

--- src/includes/ManagerApproval.vue
+++ src/includes/ManagerApproval.vue
@@ -84,49 +84,7 @@
           </v-stepper-step>
 
           <v-stepper-content step="2" class="pt-0">
-            <compose-workflow v-model="schema" />
-            <v-btn
-              @click="e6 = 3"
-              dark
-              text
-              elevation="0"
-              width="125"
-              height="55"
-              style="
-                margin-top: 26px;
-                margin-bottom: 73px;
-                background: var(--v-primary-base);
-                box-shadow: 0px 12px 22px rgba(0, 0, 0, 0.24);
-                border-radius: 4px;
-              "
-            >
-              <simple-line-icons
-                icon="arrow-right"
-                color="#FFFFFF"
-                style="
-                  font-family: simple-line-icons;
-                  font-style: normal;
-                  font-weight: normal;
-                  font-size: 16px;
-                  line-height: 16px;
-                "
-                no-svg
-              />
-              <span
-                class="text-capitalize pl-3 py-4"
-                style="
-                  font-family: Inter;
-                  font-style: normal;
-                  font-weight: 500;
-                  font-size: 14px;
-                  line-height: 17px;
-                  text-align: center;
-                  letter-spacing: 0.636364px;
-                  color: #ffffff;
-                "
-                >next</span
-              >
-            </v-btn>
+            <compose-workflow v-model="schema" @completed="e6 = 3" />
           </v-stepper-content>
 
           <v-stepper-step :editable="e6 > 3" step="3">
