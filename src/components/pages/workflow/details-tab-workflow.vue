e422aabd82895f03ab84851cd920e4c3b58ac072
Xcedeveloper <xceldeveloper@gmail.com>
2022-04-09 01:11:00 +0100

    workflow detail tab

---
+++ src/components/pages/workflow/details-tab-workflow.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-tabs
+    grow
+    slider-color="#301F78"
+    slider-size="4"
+    background-color="#fffbf1"
+    style="margin:0px auto auto auto;width:100% mix-blend-mode: normal;padding:10px"
+  >
+    <v-tab> Details </v-tab>
+    <v-tab> Audit Trail </v-tab>
+
+    <v-tab-item>
+      <v-divider></v-divider>
+      Details
+    </v-tab-item>
+    <v-tab-item>
+      <v-divider></v-divider>
+      Audit
+    </v-tab-item>
+  </v-tabs>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
