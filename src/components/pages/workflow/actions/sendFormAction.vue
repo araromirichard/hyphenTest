1906f9fae265114063b6d4e17dc3029131fa456e
Xcedeveloper <xceldeveloper@gmail.com>
2022-05-05 15:36:03 +0100

    form action

--- src/components/pages/workflow/actions/sendFormAction.vue
+++ src/components/pages/workflow/actions/sendFormAction.vue
@@ -56,7 +56,7 @@
             <v-icon left>mdi-close</v-icon> Cancel
           </v-btn>
 
-          <v-btn large color="primary" elevation="0">
+          <v-btn @click="addToWorkflow" large color="primary" elevation="0">
             <v-icon left>mdi-chevron-right</v-icon> Add to FLow
           </v-btn>
         </div>
@@ -78,6 +78,25 @@ export default {
     },
     close() {
       this.dialog = false;
+    },
+
+    addToWorkflow() {
+      const payload = {
+        type: "hyphenForm",
+        properties: {
+          keys: ["form id", "form name", "identity", "organization id", "name"],
+          values: [
+            "form id value",
+            "form name value",
+            "identity value",
+            "organization id value",
+            "form",
+          ],
+        },
+      };
+
+      this.$emit("input", payload);
+      this.close();
     },
   },
   watch: {
@@ -124,7 +143,7 @@ export default {
   }
 
   &__content {
-    background-color: #F8F7F4;
+    background-color: #f8f7f4;
     padding: 20px 50px;
 
     .top {
